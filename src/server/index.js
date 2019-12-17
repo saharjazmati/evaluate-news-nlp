3cc2598feb5f997be1030714a01bc378574299bc
Juwana Zerman <38802201+JZerman2018@users.noreply.github.com>
2019-12-16 20:21:35 -0500

    Finish environment setup

--- src/server/index.js
+++ src/server/index.js
@@ -1,22 +1,30 @@
+const dotenv = require('dotenv');
+dotenv.config();
 var path = require('path')
+var bodyParser = require('body-parser');
+var cors = require('cors');
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
-var AYLIENTextAPI = require('aylien_textapi');
+var aylienTextAPI = require('aylien_textapi');
 
-var textapi = new AYLIENTextAPI({
-  application_id: "YOUR_APP_ID",
-  application_key: "YOUR_APP_KEY"
+var textapi = new aylien({
+  application_id: process.env.API_ID,
+  application_key: process.env.API_KEY
 });
 
 const app = express()
 
-app.use(express.static('dist'))
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 
-console.log(__dirname)
+app.use(express.static('dist'));
+
+//console.log(__dirname)
 
 app.get('/', function (req, res) {
-    res.sendFile('dist/index.html')
-})
+    res.sendFile(path.resolve('dist/index.html');
+});
 
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
@@ -25,4 +33,11 @@ app.listen(8080, function () {
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
-})
+});
+
+// Post
+
+
+
+
+module.exports = app;

04029ce10a812677ff8c313261714af9a877d4aa
Juwana Zerman <38802201+JZerman2018@users.noreply.github.com>
2019-12-20 15:17:54 -0500

    Setting up API call

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,3 +1,40 @@
+import {isURL, closeError, showError, showHideResults} from ('./helpers');
+
+/* This sends the user's URL (POST request) to server */
+const sendRequest = async (url = '', data = {}) => {
+        const res = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        try {
+            const data = await res.json();
+            return data;
+        } catch (err) {
+            showError(err);
+        }
+    };
+
+/* Shows results or errors on page */
+const displayContent = (data) => {
+
+    if(data.error) {
+        showError(data.error);
+    } else {
+        //get results
+        const apiresults = document.getElementById('results');
+
+        // add results dynamically
+        
+
+    }
+}
+
+
+
+
 
 
 

4ba73da872569a19f1c9e8996fc8523c4b74800e
Juwana Zerman <38802201+JZerman2018@users.noreply.github.com>
2019-12-19 20:03:58 -0500

    Update packages and JS

--- src/server/index.js
+++ src/server/index.js
@@ -1,13 +1,87 @@
+const dotenv = require('dotenv');
+dotenv.config();
+//var path = require('path');
+var bodyParser = require('body-parser');
 
-//var aylienTextAPI = require('aylien_textapi');
+const express = require('express');
+const app = express();
+var aylienAPI = require('aylien_textapi');
 
-// var textapi = new aylien({
-//   application_id: process.env.API_ID,
-//   application_key: process.env.API_KEY
-// });
+var textapi = new aylien({
+  application_id: process.env.API_ID,
+  application_key: process.env.API_KEY
+});
 
-const app = require('./app');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(express.static('dist'));
+
+//const app = require('./app');
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
     console.log('App listening on port 8080!')
+});
+
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html');
+});
+
+// API request sent to Aylien API
+app.post('/apicall', (req, res) => {
+// result data object will send back to the user's browser
+    const results = {};
+    results.url = req.body.url;
+
+// the 1st api request is sent
+    textapi.extract({
+        url: req.body.url,
+        best_image: true
+    },
+    (errExtract, resExtract) => {
+        //check for errors, if no errors
+        if (errExtract === null) {
+            // this adds the article, title and image to the results
+            resExtract.article != '' ? results.text =
+            resExtract.article.slice(0, 300) + '...' : '';
+            resExtract.title != '' ? resultData.title = resExtract.title : '';
+            resExtract.image != '' ? resultData.image = resExtract.image : '';
+
+            if(resExtract.article != '') {
+
+          //second API request is sent if the article is not empty
+          textapi.sentiment(
+            {'text': resExtract.article},
+            (errSentiment, resSentiment) => {
+
+                //if not errors
+              if (errSentiment === null) {
+                console.log(resSentiment);
+                //add new sentiment result to resultData object
+                results.polarity = resSentiment.polarity;
+                results.subjectivity = resSentiment.subjectivity;
+                results.polarity_confidence = (resSentiment.polarity_confidence).toFixed(2);
+                results.subjectivity_confidence = (resSentiment.subjectivity_confidence).toFixed(2);
+
+                //send the results to the browser
+                res.send(resultData);
+              }
+        }
+    );
+} else {
+    //if text is empty send error message to the browser
+          res.send({
+            error: `Unfortunately, no text was found on the page<br>
+                    Please, try another URL/Web Page!`
+          });
+}
+} else {
+        //error message for bad url to the browser/user
+        res.send({
+          error: `The URL you requested was not found.<br>
+                  Please, check the URL and try again!`
+        });
+}
+    });
 });

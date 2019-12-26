a11a2540564a067a1e10ef3c3f41f4b9715faca8
Juwana Zerman <38802201+JZerman2018@users.noreply.github.com>
2019-12-25 23:28:41 -0500

    Add a test

--- src/tests/formHandler.test.js
+++ src/tests/formHandler.test.js
@@ -1,4 +1,4 @@
-import { handleSubmit } from '../client/js/formHandler';
+import { handleSubmit, fetchAylien } from '../client/js/formHandler';
 
 describe('Test, the function "handleSubmit()" should exist' , () => {
     test('It should return true', async () => {
@@ -9,5 +9,11 @@ describe('Test, the function "handleSubmit()" should exist' , () => {
 describe('Test, the function "handleSubmit()" should be a function', () => {
     test('It should be a function', async () => {
         expect (typeof handleSubmit).toBe('function');
+    });
+});
+
+describe('Test, the function "fetchAylien()" should exist', () => {
+    test('It should return true', async () => {
+        expect(fetchAylien).toBeDefined();
     });
 });
